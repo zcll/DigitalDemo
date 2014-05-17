0fe1063bd3b3fb60d390c47b64f5e64843258be4
zcll <lichongzhuang88>
2014-05-18 01:39:25 +0800

    first commit

---
+++ DigitalDemo/UtilsMacro.h
@@ -0,0 +1,14 @@
+//
+//  UtilsMacro.h
+//  DigitalDemo
+//
+//  Created by lic&z on 14-5-18.
+//  Copyright (c) 2014年 lic&z. All rights reserved.
+//
+
+#ifndef DigitalDemo_UtilsMacro_h
+#define DigitalDemo_UtilsMacro_h
+
+
+
+#endif
